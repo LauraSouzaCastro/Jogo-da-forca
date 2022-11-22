db57fe5be7de574408af71e1d4707de3f5492a19
Laura <laurasouzacastro1@gmail.com>
2022-11-21 22:01:13 -0300

    criação dos arquivos dos componentes jogo, letras e chute

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../css/App.css';
+import Jogo from './Jogo';
+import Letras from './Letras';
+import Chute from './Chute';
+
+function App() {
+  return (
+    <>
+      <Jogo />
+      <Letras />
+      <Chute />
+    </>
+  );
+}
+
+export default App;
