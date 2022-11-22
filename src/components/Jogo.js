d3201b90d9c3c46d3b35cbee1f4ae9b49cff84b6
Laura <laurasouzacastro1@gmail.com>
2022-11-21 22:29:49 -0300

    adicionado imagem, botão e palavra no componente jogo

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,5 +1,13 @@
+import '../css/Jogo.css';
+
 export default function Jogo(){
     return(
-        <></>
+        <div className='caixaJogo'>
+            <img src='./assets/forca0.png' className='forca' alt='' />
+            <div className='caixaBotaoPalavra'>
+                <button className='botao'>Escolher Palavra</button>
+                <span className='palavraJogo'>Palavra</span>
+            </div>
+        </div>
     )
 }
