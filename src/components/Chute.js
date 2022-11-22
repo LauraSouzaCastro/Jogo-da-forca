849b50724a58d33e9f4d8722880348f38ceaf7af
Laura <laurasouzacastro1@gmail.com>
2022-11-21 23:18:30 -0300

    adicionados texto, input e botão no componente chute

--- src/components/Chute.js
+++ src/components/Chute.js
@@ -1,5 +1,10 @@
+import '../css/Chute.css';
 export default function Chute(){
     return(
-        <></>
+        <div className='caixaChute'>
+            <span className='texto'>Já sei a palavra!</span>
+            <input type={'text'} className='inputChute'></input>
+            <button className='botaoChutar'>Chutar</button>
+        </div>
     )
 }
